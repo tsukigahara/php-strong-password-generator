11df52137cb437bd1b529fd4be307bff925b47b3
tsukigahara <satsuki.tksm@gmail.com>
2023-01-11 19:41:45 +0100

    fixed php

--- libs/helper.php
+++ libs/helper.php
@@ -20,10 +20,11 @@ function passwordGen($length, $isAlpha, $isNum, $isSymb)
         $string .= $symb;
     }
 
+    // declare $pass as string
     $pass = "";
 
     // create password
-    for ($i = 0; $i < $length - 1; $i++) {
+    for ($i = 0; $i < $length; $i++) {
 
         // pick random int from 0 to {$string length}
         $index = random_int(0, strlen($string));
@@ -35,7 +36,7 @@ function passwordGen($length, $isAlpha, $isNum, $isSymb)
     return $pass;
 };
 
-// get length from url
+// get parameter from url
 $length = $_GET["length"] ?? false;
 $isAlpha = $_GET["alphabet"];
 $isNum = $_GET["number"];
